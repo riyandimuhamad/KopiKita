d4cb6071b71e94ad4176cadb4212a88e6c117c8d
riyandimuhamad <riyandiiimuhammad@gmail.com>
2026-01-28 20:18:35 +0700

    Fixing

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
     use HasFactory;
+
+    // Untuk memberi izin pengisian data
+    protected $fillable = ['name', 'price', 'category', 'status'];
 }
